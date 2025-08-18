913c16a6ca8ebcef2525e6f2bd8ba3905a6dc9df
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-08-18 22:05:50 +0530

    Create CountSquares.java

---
+++ Easy/CountSquares.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    static int countSquares(int n) {
+        // code here
+        int count = (int) Math.sqrt(n - 1);
+        return count;
+    }
+}
