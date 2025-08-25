60d293e084611b89c9afbbd17d8ca3741f20109f
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-08-25 14:30:13 +0530

    Update segregateEvenOdd.java

--- Easy/segregateEvenOdd.java
+++ Easy/segregateEvenOdd.java
@@ -3,6 +3,7 @@ class Solution {
         // code here
         ArrayList<Integer> even = new ArrayList<>();
         ArrayList<Integer> odd = new ArrayList<>();
+        //Assigning even numbers to even list and odd numbers to odd list
         for(int num : arr)
         {
             if(num % 2 == 0)
@@ -10,13 +11,17 @@ class Solution {
             else
             odd.add(num);
         }
-        
+
+        //Checking if the list is empty then, returning the given array as it is (without any sorting) 
+        //as segregation needs to be done only when both even and odd numbers are present in the given array
         if(even.isEmpty() || odd.isEmpty())
         return;
-        
+
+        //sorting both the lists
         Collections.sort(even);
         Collections.sort(odd);
-        
+
+        // Merging the sorted elements to array
         int index = 0;
         for(int num : even)
         {
