26ccdad59479b1e06210e61f7d00c4bf695b17a8
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-08-30 22:11:25 +0530

    Create SumofSubarrays.java

---
+++ Medium/SumofSubarrays.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] arr) {
+        /*Brute Force Approach : O(n^2) --Actually generating subarrays
+        int totalSum = 0;
+        for(int i = 0; i < arr.length; i++){
+            int subArraySum = 0;
+            for(int j = i; j < arr.length; j++){
+                subArraySum += arr[j];
+                totalSum += subArraySum;
+            }
+        }
+        return totalSum;
+        */
+        
+    }
+}
