2ae4399e9b88ba982416184764981375e44623a4
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-09-24 20:40:12 +0530

    Update LargestPrimeFactor.java

--- Medium/LargestPrimeFactor.java
+++ Medium/LargestPrimeFactor.java
@@ -1,9 +1,7 @@
-// User function Template for Java
-
 class Solution {
     static int largestPrimeFactor(int n) {
-        // code here
-      //O(n) approach
+      //Brute force approach : O(n)
+       /*
         boolean isPrime = true;
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = 2; i <= Math.sqrt(n); i++)
@@ -34,7 +32,33 @@ class Solution {
             max = Math.max(factor, max);
         }
         return max;
+        */
         //O(sqrt(n)) approach
+        /*
+        We can directly find the largest prime factor in one pass:
+        -> Divide out factors of 2 until n is odd. Keep track of 2 if it divides.
+        -> Check odd factors from 3 … √n.
+        -> While factor divides n, divide it out and update answer.
+        -> If after the loop n > 2, then n itself is a prime (and the largest prime factor).
+            */
+        int maxPrime = 1;
         
+        // remove factors of 2
+        while(n % 2 == 0){
+            maxPrime = 2;
+            n /= 2;
+        }
+        
+        // check odd factors
+        for(int i = 3; i * i <= n; i += 2){
+            while(n % i == 0){
+                maxPrime = i;
+                n /= i;
+            }
+        }
+        
+        // if n > 2 this means that n is a prime number
+        if(n > 2)   maxPrime = n;
+        return maxPrime;
     }
 }
